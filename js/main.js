843bdc78b0a37d52cec006fbe628866049a59c7c
Alucard <134976585+realcartman@users.noreply.github.com>
2023-11-13 19:08:22 -0300

    yesssss

--- js/main.js
+++ js/main.js
@@ -2,5 +2,11 @@ const form = document.getElementById("novoItem")
 
 form.addEventListener("submit", (evento)=>{
     evento.preventDefault()
-    console.log("funcionou")
+
+    
+    console.log(evento)
+    console.log(eventoevento.target[0].value)
+    console.log(eventoevento.target[1].value)
+
+
 })
